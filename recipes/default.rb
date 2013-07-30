6826916054cc2f1e6635eebd1c950858498360a7
Christian Trabold <typo3@christian-trabold.de>
2013-07-30 23:08:06 +0200

    Improve comment

--- recipes/default.rb
+++ recipes/default.rb
@@ -16,6 +16,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# Install the Graylog2 server component by default
+# Install Elasticsearch and the Graylog2 server component by default
 
 include_recipe "graylog2::server"
